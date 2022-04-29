c01ea79f0f22a795d895c94abff233684273466b
Joseph <josephskhella@gmail.com>
2022-04-29 15:19:54 +0200

    test ❗

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,7 @@ export const environment = {
   // baseUrl: 'https://ec2-3-65-226-189.eu-central-1.compute.amazonaws.com/',
 };
 
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
