120ece74ecbe4a36e6fdc741ca7f427adfd3d1a9
Joseph Safwat Khella <josephskhella@gmail.com>
2022-06-12 15:15:59 +0200

    Fix refresh token 🔃

--- src/app/models/instructors.service.ts
+++ src/app/models/instructors.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { BehaviorSubject, map, Observable, of } from 'rxjs';
+import { BehaviorSubject, delay, map, Observable, of, retryWhen } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import * as $ from "jquery";
 
@@ -18,7 +18,7 @@ export class InstructorService {
 
     return this.httpClient
       .get(`${localStorage.getItem('baseUrl')}instructor`, httpOptions)
-      .pipe(map((res) => res));
+      .pipe(map((res) => res), retryWhen((errors) => errors.pipe(delay(2000))));
   }
 
   createInstructor(instructor: any) {
